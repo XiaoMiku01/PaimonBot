68d9d7cab2369db8162e6436e8a111aec0f9edf4
Xiao-MiKu <54094119+XiaoMiku01@users.noreply.github.com>
2021-03-13 12:35:21 -0600

    bot主文件

---
+++ bot.py
@@ -0,0 +1,12 @@
+from os import path
+
+
+import nonebot
+import config
+if __name__ == '__main__':
+    nonebot.init(config)
+    nonebot.load_plugins(
+        path.join(path.dirname(__file__), 'awesome', 'plugins'),
+        'awesome.plugins'
+    )
+    nonebot.run(host='127.0.0.1', port=9898)
---
+++ config.py
@@ -0,0 +1,4 @@
+from nonebot.default_config import *
+import  re
+SUPERUSERS = {}
+COMMAND_START = ['', re.compile(r'[/!]+')]
