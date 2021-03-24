983571518a7ec6186b2727ce443a345974d68e48
晓轩 <54094119+XiaoMiku01@users.noreply.github.com>
2021-03-24 17:03:18 +0800

    路径换成绝对路径

--- awesome/plugins/chat/__init__.py
+++ awesome/plugins/chat/__init__.py
@@ -7,8 +7,14 @@ other = rcnb.CommandGroup('other', only_to_me=False)
 
 @other.command('chat')
 async def _(session: rcnb.CommandSession):
-    await session.finish("派蒙现在还听不懂哦，\n@我 help ，获取功能哦！[CQ:image,file=90aa3d3a9635b56bf4c4d00b436f5981.image,url=http://c2cpicdw.qpic.cn/offpic_new/1561900932//1561900932-3987012577-90AA3D3A9635B56BF4C4D00B436F5981/0?term=3]")
+    await session.finish("派蒙现在还听不懂哦，\n@我 help ，获取功能哦！[CQ:image,file=file:///www/wwwroot/PaimonBot/Bot/face/face1.png]")
     return
+
+
+# @other.command('chat')
+# async def _(session: rcnb.CommandSession):
+#     await session.finish("派蒙正在适应香港哦，所有功能可能失效几天！！[CQ:image,file=90aa3d3a9635b56bf4c4d00b436f5981.image,url=https://uploadstatic.mihoyo.com/ys-obc/2020/03/12/4820086/701d98b82183919e58afcef508981cbc_2599105281229745644.png?x-oss-process=image/quality,q_70/resize,p_80]")
+#     return
 
 
 @rcnb.on_natural_language()
