b32e08b4ba8ab9eb0f2b4ec45505fbc91fc9ec83
晓轩 <54094119+XiaoMiku01@users.noreply.github.com>
2021-03-31 11:42:41 +0800

    重写init

--- awesome/plugins/mys/__init__.py
+++ awesome/plugins/mys/__init__.py
@@ -1,24 +1,13 @@
 from nonebot import on_command, CommandSession
 
-from .data_source import get_weather_of_city
+from .getImg import draw_pic
 
-@on_command('查询')
+@on_command('查询',only_to_me=False,aliases=('派蒙查询'))
 async def weather(session: CommandSession):
-    uid = session.get('uid',prompt='你想查询哪个uid呢？')
-    weather_report = await get_weather_of_city(uid)
-    await session.send(weather_report)
 
-
-@weather.args_parser
-async def _(session: CommandSession):
     stripped_arg = session.current_arg_text.strip()
-
-    if session.is_first_run:
-        if stripped_arg:
-            session.state['uid'] = stripped_arg
-        return
-
-    if not stripped_arg:
-        session.pause('要查询的uid不能为空呢，请重新输入')
-
+    session.state['uid'] = stripped_arg
     session.state[session.current_key] = stripped_arg
+    uid = session.get('uid')
+    im = await draw_pic(uid)
+    await session.send(f'[CQ:image,file={im}]')
